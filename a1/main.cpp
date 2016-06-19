4078b1b355c2a303e20dbc902bbebf5cf0a76443
Colin Murphy <colinmurphy97@gmail.com>
2016-06-19 15:22:17 -0700

    a1 finished, a2 in progress

--- a1/main.cpp
+++ a1/main.cpp
@@ -1,3 +1,20 @@
+/*
+A1
+Colin Murphy
+CIS 278
+Dave Harden
+6/17/16
+
+    This is a simple Blackjack program that generates 2 numbers between 1 and 10,
+and adds the 2 together. The user is then prompted whether or not they want another card.
+If the input is 'y' another random number between 1 and 10 is generated and a new total is
+calculated and outputed. If the total equal to 21, then 'congratulations' is outputed and
+the user is prompted to decide whether or not to play again. If the total is greater than 21,
+then 'bust' is outputed and the user is prompted to decide whether or not to play again.
+If neither is true then the user decides whether they want another card.
+*/
+
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -9,51 +26,94 @@ int main()
 {
     srand(time(0));
 
-    int card1 = (rand() % 10) + 1;
-    int card2 = (rand() % 10) + 1;
+    int card1;
+    int card2;
     char addCardChoice;
     int addCardValue;
-    int total = card1 + card2;
+    int total;
     char playChoice;
-    bool play = true;
+    bool repeat;
 
-    cout << "First cards: " << card1 << ", " << card2 << endl;
-    cout << "Total: " << total << endl;
-    cout << "Do you want another card? (y, n) : ";
-    cin >> addCardChoice;
-
-    while (play)
+    do
     {
-        while (total < 21)
+        card1 = (rand() % 10) + 1;
+        card2 = (rand() % 10) + 1;
+        total = card1 + card2;
+
+        cout << "First cards: " << card1 << ", " << card2 << endl;
+        cout << "Total: " << total << endl;
+        cout << "Do you want another card? (y, n) : ";
+        cin >> addCardChoice;
+        cout << "You entered: " << addCardChoice << endl;
+
+
+
+        while (total < 21 && addCardChoice == 'y')
         {
-            if (addCardChoice == 'y')
+            addCardValue = (rand() % 10) + 1;
+            total = total + addCardValue;
+
+            cout << "Card: " << addCardValue << endl;
+            cout << "Total: " << total << endl;
+
+            if (total == 21)
             {
-                addCardValue = (rand() % 10) + 1;
-                total = total + addCardValue;
-
-                cout << "Card: " << addCardValue << endl;
-                cout << "Total: " << total << endl;
-
-                if (total >= 21)
-                {
-                    cout << "Busted!" << endl;
-                }
-                else
-                {
-                    cout << "Do you want another card? (y, n) : ";
-                    cin >> addCardChoice;
-                }
+                cout << "Congratulations" << endl;
+            }
+            else if (total > 21)
+            {
+                cout << "Bust" << endl;
+            }
+            else
+            {
+                cout << "Do you want another card? (y, n) : ";
+                cin >> addCardChoice;
+                cout << "You entered: " << addCardChoice << endl;
             }
         }
 
         cout << "Do you want to play again?(y, n) : ";
         cin >> playChoice;
-        if (playChoice =! 'y')
+
+        if (playChoice == 'y')
         {
-            play = false;
+            repeat = true;
         }
-    }
+        else
+        {
+            repeat = false;
+        }
+    }while (repeat);
 
 
     return 0;
 }
+
+/*
+
+First cards: 4, 10
+Total: 14
+Do you want another card? (y, n) : y
+You entered: y
+Card: 10
+Total: 24
+Bust
+Do you want to play again?(y, n) : y
+First cards: 1, 3
+Total: 4
+Do you want another card? (y, n) : y
+You entered: y
+Card: 4
+Total: 8
+Do you want another card? (y, n) : y
+You entered: y
+Card: 10
+Total: 18
+Do you want another card? (y, n) : n
+You entered: n
+Do you want to play again?(y, n) : n
+
+Process returned 0 (0x0)   execution time : 10.921 s
+Press any key to continue.
+
+*/
