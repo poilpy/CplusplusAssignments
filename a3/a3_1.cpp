a9a8914e7a721dceb07fb2ced4b07003fee8248a
poilpy <ColinMurphy97@gmail.com>
2016-06-25 17:44:06 -0700

    a3 in progress

--- a3/a3_1.cpp
+++ a3/a3_1.cpp
@@ -14,13 +14,66 @@ const int HAND_SIZE = 5;
 
 using namespace std;
 
-void createHand(int *&hand[])
+bool containsPair(const int hand[]);
+bool containsTwoPair(const int hand[]);
+bool containsThreeOfaKind(const int hand[]);
+bool containsStraight(const int hand[]);
+bool containsFullHouse(const int hand[]);
+bool containsFourOfaKind(const int hand[]);
+int sameVal(const int hand[]);
+int valCount(int val, const int hand[]);
+int secondCheck(int val, const int hand[]);
+
+
+int main()
+
 {
+    int hand[HAND_SIZE];
 
+    cout << "Enter five numeric cards, no face cards. Use 2 - 9." << endl;
 
+    for(int i = 0; i < HAND_SIZE; i++)
+    {
+        cin >> hand[i];
+        cout << "Added : " << hand[i] << endl;
+    }
 
+    if(containsFourOfaKind(hand))
+    {
+        cout << "Four of a kind!" << endl;
+    }
+    else if(containsFullHouse(hand))
+    {
+        cout << "Full house!" << endl;
+    }
+    else if(containsStraight(hand))
+    {
+        cout << "Straight!" << endl;
+    }
+    else if(containsThreeOfaKind(hand))
+    {
+        cout << "Three of a kind!" << endl;
+    }
+    else if(containsTwoPair(hand))
+    {
+        cout << "Two pair!" << endl;
+    }
+    else if (containsPair(hand))
+    {
+        cout << "Pair!" << endl;
+    }
+    else
+    {
+        cout << "High Card!" << endl;
+    }
+
+    for(int i = 0; i < HAND_SIZE; i++)
+    {
+        cout << hand[i] << " ";
+    }
+
+    return 0;
 }
-
 
 
 
@@ -28,7 +81,12 @@ void createHand(int *&hand[])
 
 bool  containsPair(const int hand[])
 {
-
+    if(sameVal(hand) == 2)
+    {
+        return true;
+    }
+    else
+        return false;
 }
 
 
@@ -38,7 +96,12 @@ bool  containsPair(const int hand[])
 
 bool  containsTwoPair(const int hand[])
 {
-
+    if(sameVal(hand) == 2 && secondCheck())
+    {
+        return true;
+    }
+    else
+        return false;
 }
 
 
@@ -48,7 +111,12 @@ bool  containsTwoPair(const int hand[])
 
 bool  containsThreeOfaKind(const int hand[])
 {
-
+    if(sameVal(hand) == 3)
+    {
+        return true;
+    }
+    else
+        return false;
 }
 
 
@@ -58,7 +126,7 @@ bool  containsThreeOfaKind(const int hand[])
 
 bool  containsStraight(const int hand[])
 {
-
+    return false;
 }
 
 
@@ -68,7 +136,7 @@ bool  containsStraight(const int hand[])
 
 bool  containsFullHouse(const int hand[])
 {
-
+    return false;
 }
 
 
@@ -78,7 +146,31 @@ bool  containsFullHouse(const int hand[])
 
 bool  containsFourOfaKind(const int hand[])
 {
+    if(sameVal(hand) == 4 || sameVal(hand) == 5)
+    {
+        return true;
+    }
+    else
+        return false;
+}
 
+
+
+
+
+int valCount(int val, const int hand[])
+{
+    int sameCount = 0;
+
+    for(int i = 0; i < HAND_SIZE; i++)
+    {
+        if(hand[i] == hand[val])
+        {
+            sameCount++;
+        }
+    }
+
+    return sameCount;
 }
 
 
@@ -86,26 +178,36 @@ bool  containsFourOfaKind(const int hand[])
 
 
 
-int main() {
+int sameVal(const int hand[])
 {
-    int hand[HAND_SIZE];
-
-    cout << "Enter five numeric cards, no face cards. Use 2 - 9." << endl;
+    int highest = 0;
 
     for(int i = 0; i < HAND_SIZE; i++)
     {
-        cin >> hand[i];
-        cout << hand[i];
+        if(valCount(i, hand) > highest)
+        {
+            highest = valCount(i, hand);
+        }
+    }
+    return highest;
+}
+
+
+
+
+
+
+int secondCheck(int val, const int hand[])
+{
+    int highest = 0;
+
+    for(int i = val; i < HAND_SIZE; i++)
+    {
+        if(valCount(i, hand) > highest)
+        {
+            highest = valCount(i, hand);
+        }
     }
 
-    if (containsAPair(hand))
-    {
-        cout << "contains a pair" << endl;
-    }
-    else
-    {
-        cout << "High Card" << endl;
-    }
-
-    return 0;
+    return highest;
 }
