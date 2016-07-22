1bfda3f4e27ec3415ff9af82bfa5b4429027d5f3
Colin Murphy <colinmurphy97@gmail.com>
2016-07-22 02:04:20 -0700

    a10 finished, a11 in progress

--- a10/include/myString.h
+++ a10/include/myString.h
@@ -1,16 +1,48 @@
 #ifndef MYSTRING_H
 #define MYSTRING_H
 
+#include <iostream>
+#include <cstring>
+using namespace std;
 
+namespace cs_mystring
+{
 class myString
 {
-    public:
-        myString();
-        virtual ~myString();
+public:
+    myString(const char* create = "");
+    virtual ~myString();
+    myString(const myString& right);
+    myString& operator=(const myString& right);
+    char& operator[](int i);
+    char operator[](int i)const;
+    friend ostream& operator<<(ostream& out,
+                               const myString& right);
+    friend istream& operator>>(istream& in,
+                               const myString& right);
+    friend myString& operator+(myString& left,
+                               myString& right);
+    friend myString& operator+=(myString& left,
+                               myString& right);
+    friend bool operator<(const myString& left,
+                          const myString& right);
+    friend bool operator<=(const myString& left,
+                           const myString& right);
+    friend bool operator>=(const myString& left,
+                           const myString& right);
+    friend bool operator>(const myString& left,
+                          const myString& right);
+    friend bool operator==(const myString& left,
+                           const myString& right);
+    friend bool operator!=(const myString& left,
+                           const myString& right);
+    void read(istream& in,
+              char stop);
 
-    protected:
+    int length()const;
 
-    private:
+private:
+    char *array;
 };
-
+}
 #endif // MYSTRING_H
--- a10/src/myString.cpp
+++ a10/src/myString.cpp
@@ -1,11 +1,198 @@
 #include "myString.h"
+#include <assert.h>
 
-myString::myString()
+namespace cs_mystring
 {
-    //ctor
+myString::myString(const char* create)
+{
+    array = new char[strlen(create) + 1];
+    strcpy(array, create);
 }
+
+
+
+
+
+
 
 myString::~myString()
 {
-    //dtor
+    delete[] array;
+}
+
+
+
+
+
+
+
+myString::myString(const myString& right)
+{
+    array = new char[strlen(right.array) + 1];
+    strcpy(array, right.array);
+}
+
+
+
+
+
+
+myString& myString::operator=(const myString& right)
+{
+    if (this != &right)
+    {
+        delete [] array;
+        array = new char[strlen(right.array) + 1];
+        strcpy(array, right.array);
+    }
+    return *this;
+}
+
+
+
+
+
+
+
+ostream& operator<<(ostream& out, const myString& right)
+{
+    for(int i = 0; i < strlen(right.array); i++)
+    {
+        out << right.array[i];
+    }
+    return out;
+}
+
+
+
+
+
+
+istream& operator>>(istream& out, const myString& right)
+{
+
+}
+
+
+
+
+
+
+myString& operator+(const myString& left, const myString& right)
+{
+
+}
+
+
+
+
+
+myString& operator+=(const myString& left, const myString& right)
+{
+
+}
+
+
+
+
+
+
+
+char myString::operator[](int i)const
+{
+    assert(i >= 0 && i < strlen(array));
+
+    return array[i];
+}
+
+
+
+
+
+
+char& myString::operator[](int i)
+{
+    assert(i >= 0 && i < strlen(array));
+
+    return array[i];
+}
+
+
+
+
+
+
+bool operator<(const myString& left, const myString& right)
+{
+    return strcmp(left.array, right.array) < 0;
+}
+
+
+
+
+
+
+bool operator<=(const myString& left, const myString& right)
+{
+    return strcmp(left.array, right.array) < 0 || strcmp(left.array, right.array) == 0;
+}
+
+
+
+
+
+
+bool operator>=(const myString& left, const myString& right)
+{
+    return strcmp(left.array, right.array) > 0 || strcmp(left.array, right.array) == 0;
+}
+
+
+
+
+
+
+bool operator>(const myString& left, const myString& right)
+{
+    return strcmp(left.array, right.array) > 0;
+}
+
+
+
+
+
+
+bool operator==(const myString& left, const myString& right)
+{
+    return strcmp(left.array, right.array) == 0;
+}
+
+
+
+
+
+
+bool operator!=(const myString& left, const myString& right)
+{
+    return strcmp(left.array, right.array) != 0;
+}
+
+
+
+
+
+void myString::read(istream& in, char stop)
+{
+
+}
+
+
+
+
+
+
+int myString::length()const
+{
+    return strlen(array);
+}
 }
